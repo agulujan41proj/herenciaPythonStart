07277f93f379aec7eeaf6cc3dc6cf1241fa69f9e
agulujan41proj <119899671+agulujan41proj@users.noreply.github.com>
2024-03-09 12:36:43 -0300

    Update juego.py

--- juego.py
+++ juego.py
@@ -9,16 +9,27 @@ class Sprite(Turtle):
         self.shape(shape)
         self.step=step
     def mover_arriba(self):
-        print("arriba")
+        self.goto(self.xcor(),self.ycor()+self.step)
     def mover_abajo(self):
-        print("abajo")
+        self.goto(self.xcor(),self.ycor()-self.step)
     def mover_derecha(self):
-        print("derecha")
+        self.goto(self.xcor()+self.step,self.ycor())
     def mover_izquierda(self):
-        print("izquierda")
+        self.goto(self.xcor()-self.step,self.ycor())
+
+    def hay_contacto(self,sprite1):
+        distancia = self.distance(sprite1.xcor(),sprite1.ycor())
+        if distancia < 30:
+            return True
+        else:
+            return False
 
 
 player = Sprite(0,-100,10,"circle","orange")
+enemigo1 =Sprite(-200,0,10,"square","red")
+enemigo2 =Sprite(200,20,10,"square","red")
+meta=Sprite(0,120,10,"triangle","green")
+
 pantalla = player.getscreen()
 pantalla.listen()
 
@@ -26,5 +37,19 @@ pantalla.onkey(player.mover_arriba,"Up")
 pantalla.onkey(player.mover_abajo,"Down")
 pantalla.onkey(player.mover_derecha,"Right")
 pantalla.onkey(player.mover_izquierda,"Left")
+
+
+total_score =0
+
+while total_score< 3:
+    if player.hay_contacto(meta):
+        player.goto(0,-100)
+        total_score+=1
+    if player.hay_contacto(enemigo1) or player.hay_contacto(enemigo2):
+        meta.hideturtle()
+
+enemigo1.hideturtle()
+enemigo2.hideturtle()
+
 
 exitonclick()
